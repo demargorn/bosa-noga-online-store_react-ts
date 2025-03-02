0ea8b3b4dc7ba98d91a4330d8debe38355604d13
Михаил Дорошин <demargorn@gmail.com>
2025-03-02 20:26:26 +0300

    cart slice added

--- src/slices/cart.slice.ts
+++ src/slices/cart.slice.ts
@@ -1,18 +1,40 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IItem } from '../interfaces/Item.interface';
 
-const initialState = {
+export interface IState {
+   items: Array<IItem>;
+   hits: Array<IItem>;
+}
+
+const initialState: IState = {
    items: [],
+   hits: [],
 };
 
 const cartSlice = createSlice({
    name: 'cart',
    initialState,
    reducers: {
-      add: (state, { payload }: PayloadAction<number>) => {
+      getItems: (state, { payload }: PayloadAction<IItem>) => {
+         const existed = state.items.find((i) => i.id === payload.id); // ищем совпадения по id
+         if (existed) {
+            return;
+         }
          state.items.push(payload);
+      },
+      removeItem: (state, { payload }: PayloadAction<IItem>) => {
+         state.items = state.items.filter((item) => item.id !== payload.id);
+      },
+      //загружаем список хитов
+      getHits: (state, { payload }: PayloadAction<IItem>) => {
+         const existed = state.hits.find((i) => i.id === payload.id); // ищем совпадения по id
+         if (existed) {
+            return;
+         }
+         state.hits.push(payload);
       },
    },
 });
 
-export const {} = cartSlice.actions;
+export const { getItems, getHits } = cartSlice.actions;
 export default cartSlice.reducer;
