296d086ba6c4c29df169845208e8a9ee7a6586e5
Михаил Дорошин <demargorn@gmail.com>
2025-03-04 19:47:07 +0300

    items height fixed

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,12 +1,13 @@
 import { useNavigate } from 'react-router';
 import { IItem } from '../../interfaces/Item.interface';
 import Button from '../Button/Button';
+import './Item.css'
 
 const Item = (props: IItem) => {
    const navigate = useNavigate();
 
    return (
-      <div className='col-4'>
+      <div className='col-4 item' >
          <div className='card catalog-item-card'>
             <img src={props.images[0]} className='card-img-top img-fluid' alt={props.title} />
             <div className='card-body'>
