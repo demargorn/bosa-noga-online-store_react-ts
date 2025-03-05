58d4ba05629aef54745987f7b51e83c1e5dd887d
Михаил Дорошин <demargorn@gmail.com>
2025-03-05 17:23:20 +0300

    products.slice fixed

--- src/slices/products.slice.ts
+++ src/slices/products.slice.ts
@@ -15,27 +15,31 @@ const productsSlice = createSlice({
    name: 'products',
    initialState,
    reducers: {
-      getItems: (state, { payload }: PayloadAction<IItem>) => {
-         const existed = state.items.find((i) => i.id === payload.id); // ищем совпадения по id
-         // запрещаем добавление одинаковых элементов
-         // if (existed) {
-         //    return;
-         // }
-
-         state.items.push(payload);
-      },
-
       //загружаем список хитов
-      getHits: (state, { payload }: PayloadAction<IItem>) => {
-         const existed = state.hits.find((i) => i.id === payload.id); // ищем совпадения по id
-         // запрещаем добавление одинаковых элементов
-         if (existed) {
-            return;
-         }
-         state.hits.push(payload);
+      getHits: (state, { payload }: PayloadAction<Array<IItem>>) => {
+         payload.map((p) => {
+            const existed = state.hits.find((i) => i.id === p.id); // ищем совпадения по id
+
+            if (existed) {
+               return; // запрещаем добавление одикаковых по id элементов
+            }
+            state.hits.push(...payload);
+         });
       },
       clear: (state) => {
          state.items = [];
+      },
+
+      //загружаем список каталога
+      getItems: (state, { payload }: PayloadAction<Array<IItem>>) => {
+         payload.map((p) => {
+            const existed = state.items.find((i) => i.id === p.id); // ищем совпадения по id
+
+            if (existed) {
+               return; // запрещаем добавление одикаковых по id элементов
+            }
+            state.items.push(...payload);
+         });
       },
    },
 });
