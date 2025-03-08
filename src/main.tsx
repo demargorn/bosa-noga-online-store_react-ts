a24280e408b51f708fff014f00c1b0aa46ee2ba8
Михаил Дорошин <demargorn@gmail.com>
2025-03-08 21:44:19 +0300

    bug fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { Provider } from 'react-redux';
@@ -5,8 +6,10 @@ import store from './store/store';
 import router from './router/router';
 import './main.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-   <Provider store={store}>
-      <RouterProvider router={router} />
-   </Provider>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+   <StrictMode>
+      <Provider store={store}>
+         <RouterProvider router={router} />
+      </Provider>
+   </StrictMode>
 );
